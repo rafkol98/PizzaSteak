24a0bc08810bb6e273b14324992fd40692482660
rafaelkoll <rafcall98@hotmail.com>
2021-07-12 12:45:23 +0300

    Updates

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 header = st.beta_container()
 body = st.beta_container()
 
+
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(img, img_shape=224):
   """
@@ -28,10 +29,14 @@ def load_and_prep_image(img, img_shape=224):
 
 with header:
     st.title('🍕Pizza or 🥩Steak')
-    st.write('This is an application I quickly developed to test and deploy a Deep Learning model I created which classifies images between my two different foods! Pizza and Steak')
+    st.write('This is an application I quickly developed to test and deploy a Convolutional Neural Network model I created which classifies images between my two different foods, Pizza and Steak!')
 with body:
+    display = st.checkbox('CNN model code')
+    if display:
+      st.write("The final deep learning model which drives this web application can be found here: https://colab.research.google.com/drive/1a-zdcBqSWqbm6PsiDoYC_8MyXTC6vmhS?usp=sharing. The final model was developed after extensive experimentation.")
+
     loaded_model = tf.keras.models.load_model("pizza_steak.h5")
-    uploaded_file = st.file_uploader("Choose a file")
+    uploaded_file = st.file_uploader(label="Choose a file", type=['png','jpg','jpeg'])
     if uploaded_file is not None:
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
@@ -44,4 +49,7 @@ with body:
 
         # pizza = load_and_prep_image(resized)
         pred = loaded_model.predict(tf.expand_dims(resized, axis=0))
-        st.write(pred)
+
+        label = "Mamma mia! that looks like a delicious Pizza 🍕" if pred <= 0.5 else "That looks like a juicy Steak 🥩, maybe try it with Jack Daniels sauce?" 
+
+        st.subheader(label)
